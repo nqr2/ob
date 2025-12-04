f72b8ba029b509d95a804e19c6f7e0defb9ad91a
nqr2 <naqr2@proton.me>
2025-12-04 09:22:39 -0300

    Add a REPL.

--- bin/src/Main.c
+++ bin/src/Main.c
@@ -47,6 +47,41 @@ void dofile(Context ctx, const char *path) {
   fclose(file);
 }
 
+void repl(Context ctx) {
+  auto line = (Array){};
+  arr_init(&line, ctx->allocator);
+
+  arr_reserve(&line, 256);
+
+  while (true) {
+    arr_clear(&line);
+
+    if (feof(stdin)) {
+      break;
+    }
+
+    printf("* ");
+    fflush(stdout);
+
+    int tmp = 0;
+    while (tmp != '\n') {
+      tmp = getchar();
+
+      if (tmp == -1) {
+        putchar('\n');
+        break;
+      }
+
+      auto tmp2 = (char)tmp;
+      arr_push(&line, sizeof(char), &tmp2);
+    }
+
+    run_file(ctx, line.size, line.data);
+  }
+
+  arr_free(&line);
+}
+
 int main(int argn, char *argv[]) {
   auto alloc = get_libc_allocator();
 
@@ -70,6 +105,7 @@ int main(int argn, char *argv[]) {
       dofile(ctx, argv[i]);
     }
   } else {
+    repl(ctx);
     // run a repl
   }
 
