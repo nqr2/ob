f58b4c7154a0281384419c869aa21f04e73a673e
nqr2 <naqr2@proton.me>
2026-02-01 19:03:12 -0300

    build: Clean up the code for the "snapshot" tests.

--- generate_snapshots.sh
+++ generate_snapshots.sh
@@ -2,22 +2,24 @@
 
 BUILD_DIRECTORY='.build'
 
-cmake --workflow --preset debug
+if [ ! -e $BUILD_DIRECTORY/bin/ob ]
+then
+  cmake --workflow --preset debug
+fi
 
 for f in tests/entries/*.ob
 do
   echo "Generating snapshots for $f"
 
-  basename="$(basename $f)"
-  prefix="tests/entries"
+  basename="$(basename -s .ob $f)"
 
-  if [ ! -e $prefix/basename.stdout ]
+  if [ ! -e tests/stdout/$basename ]
   then
-    $BUILD_DIRECTORY/bin/ob --verbose=0 $f --capture-stdout $prefix/$basename.stdout
+    $BUILD_DIRECTORY/bin/ob --verbose=0 $f --capture-stdout tests/stdout/$basename
   fi
 
-  if [ ! -e $prefix/basename.stderr ]
+  if [ ! -e tests/stderr/$basename ]
   then
-    $BUILD_DIRECTORY/bin/ob --verbose=0 $f --capture-stderr $prefix/$basename.stderr
+    $BUILD_DIRECTORY/bin/ob --verbose=0 $f --capture-stderr tests/stderr/$basename
   fi
 done
