242f7cee52a000be50e20edbec0d6c0e2683d4e5
nqr2 <naqr2@proton.me>
2026-02-15 00:28:23 -0300

    build: Compile the "standard library" in a single target.

--- std/src/World.c
+++ std/src/World.c
@@ -5,23 +5,11 @@
 #include <ob/core/Object.h>
 #include <ob/core/String.h>
 
-static const uint8_t SHELL[] = {
-#embed "Shell.obc"
+static const uint8_t WORLD[] = {
+#embed "World.obc"
 };
-
-static const uint8_t BOOLEAN[] = {
-#embed "Boolean.obc"
-};
-
-static void run(ob_Ctx ctx, unsigned long length, const unsigned char *data) {
-  ob_run(ctx, length, (void *)data);
-}
-
-#define RUN(C, M) run((C), sizeof(M), (M))
 
 void oblib_load_world(ob_Ctx ctx) {
-  RUN(ctx, SHELL);
-  RUN(ctx, BOOLEAN);
-
+  ob_run(ctx, sizeof(WORLD), WORLD);
   ql_array_clear(&ctx->stack);
 }
