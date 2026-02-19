a80d47799e038c57014e79ebabba991afddc4b7e
nqr2 <naqr2@proton.me>
2026-02-19 19:45:54 -0300

    Merge back the ql library (TODO: Fix)
    This reverts commit 12da593314f449d919acaf5e0975d3e9834e4880.

---
+++ ql/src/Argparse.c
@@ -0,0 +1,114 @@
+#include <ql/Argparse.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+ql_Flag ql_create_flag(char short_name, const char *long_name, ql_FlagType kind,
+                       void *pointer) {
+  return (ql_Flag){
+      .short_name = short_name,
+      .long_name = long_name,
+      .description = NULL,
+      .type = kind,
+      .target = pointer,
+  };
+}
+
+ql_Parser ql_create_parser(const ql_Flag *flags) {
+  size_t length = 0;
+
+  if (flags != NULL) {
+    for (length = 0;; length++) {
+      if ((flags[length].long_name == NULL) &&
+          (flags[length].short_name == 0)) {
+        break;
+      }
+    }
+  }
+
+  return (ql_Parser){.description = NULL, .length = length, .flags = flags};
+}
+
+void run_flag(const ql_Flag *flag, size_t *idx, const char *next_arg,
+              const ql_Parser **parser) {
+  switch (flag->type) {
+  case QL_FLAG_SET:
+    *(bool *)(flag->target) = true;
+    break;
+  case QL_FLAG_UNSET:
+    *(bool *)(flag->target) = false;
+    break;
+
+  case QL_FLAG_INT:
+    *idx += 1;
+    *(int *)(flag->target) = atoi(next_arg);
+    break;
+
+  case QL_FLAG_STRING:
+    *idx += 1;
+    *(const char **)(flag->target) = next_arg;
+    break;
+
+  case QL_FLAG_SUBCOMMAND:
+    *parser = flag->target;
+    break;
+  }
+}
+
+static const ql_Flag *parse_arg(const ql_Parser *parser, const char *arg,
+                                const char **next) {
+  for (size_t i = 0; i < parser->length; i++) {
+    auto flag = &parser->flags[i];
+
+    if (arg[0] == '-') {
+      if ((flag->long_name != NULL) && (arg[1] == '-')) {
+        auto len = strlen(flag->long_name);
+
+        if (strncmp(flag->long_name, arg + 2, len) == 0) {
+          const char *val = memchr(arg, '=', strlen(arg));
+
+          if (val != NULL) {
+            *next = val + 1;
+          }
+
+          return flag;
+        }
+      }
+
+      if ((flag->short_name != 0) && (arg[1] == flag->short_name)) {
+        if (arg[2] != 0) {
+          *next = arg + 2;
+        }
+
+        return flag;
+      }
+    }
+  }
+
+  if (arg[0] != '-' && (parser->positional_arg != NULL)) {
+    return (const ql_Flag *)1;
+  }
+
+  return NULL;
+}
+
+size_t ql_parse(const ql_Parser *parser, size_t length, const char **args) {
+  for (size_t i = 1; i < length; i++) {
+    if (parser == NULL) {
+      return i;
+    }
+
+    const char *next = args[i + 1];
+    auto flag = parse_arg(parser, args[i], &next);
+
+    if (flag == (const ql_Flag *)1) {
+      parser->positional_arg(parser->userdata, args[i]);
+    } else if (flag != NULL) {
+      run_flag(flag, &i, next, &parser);
+    } else {
+      return i;
+    }
+  }
+
+  return length;
+}
---
+++ ql/src/Tap.c
@@ -0,0 +1,114 @@
+#include <ql/Tap.h>
+
+#include <setjmp.h>
+#include <stdio.h>
+#include <threads.h>
+
+enum {
+  FAIL = -1,
+  SKIP = -2,
+  BAILOUT = -3,
+};
+
+jmp_buf thisbuf;
+const char *thisreason;
+
+void ql_skip() {
+  longjmp(thisbuf, SKIP);
+}
+
+void ql_skip_with(const char *reason) {
+  thisreason = reason;
+  ql_skip();
+}
+
+void ql_fail() {
+  longjmp(thisbuf, FAIL);
+}
+
+void ql_fail_with(const char *reason) {
+  thisreason = reason;
+  ql_fail();
+}
+
+void ql_bailout() {
+  longjmp(thisbuf, BAILOUT);
+}
+
+bool runtest(const ql_Test *test, int index) {
+  thisreason = NULL;
+
+  auto pass = true;
+  auto skipped = false;
+
+  switch (setjmp(thisbuf)) {
+  case FAIL:
+    pass = false;
+    break;
+  case SKIP:
+    skipped = true;
+    break;
+  case BAILOUT:
+    puts("Bail out!");
+    return false;
+  case 0: {
+    test->body();
+  } break;
+  }
+
+  if (!pass && test->should_fail) {
+    pass = true;
+  }
+
+  if (!pass) {
+    printf("not ");
+  }
+
+  printf("ok %d - %s", index + 1, test->name);
+
+  if (test->should_fail || skipped) {
+    printf(" #");
+
+    if (test->should_fail) {
+      printf(" TODO");
+    } else if (skipped) {
+      printf(" SKIP");
+    }
+  }
+
+  if (thisreason != NULL) {
+    printf(" # %s", thisreason);
+  }
+
+  putchar('\n');
+
+  return pass;
+}
+
+bool ql_test(const ql_Test *suite) {
+  int count = 0;
+
+  for (;; count++) {
+    if (suite[count].body == NULL) {
+      break;
+    }
+  }
+
+  printf("TAP version 13\n1..%d\n", count);
+
+  auto passed = true;
+
+  for (int i = 0;; i++) {
+    if (suite[i].body == NULL) {
+      break;
+    }
+
+    auto this_passed = runtest(&suite[i], i);
+
+    if (!this_passed) {
+      passed = false;
+    }
+  }
+
+  return passed;
+}
