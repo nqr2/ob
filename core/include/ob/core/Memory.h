1612452330d538ec930e0518860b6241a7d987c2
nqr2 <naqr2@proton.me>
2026-02-19 18:46:42 -0300

    core/mem: Document and implement stuff. Also sync doc/Doxyfile.in with the one for Read the Docs.

--- core/include/ob/core/Memory.h
+++ core/include/ob/core/Memory.h
@@ -9,10 +9,7 @@
 
 #include <ob/bits/Begin.h>
 
-#include <ql/Allocator.h>
-#include <ql/Array.h>
-#include <ql/Assert.h>
-
+#include <stddef.h>
 #include <stdint.h>
 
 /*
@@ -27,59 +24,126 @@
  * though.
  */
 
+/// A virtual memory address.
 typedef uint64_t ob_Address;
 
+/// A memory allocation hint.
 typedef enum {
-  /// The allocation is short-lived.
+  /// The allocation is for short-lived data.
   OB_MEM_TEMPORARY,
 
-  /// The allocation is for a longer-lived data structure.
+  /// The allocation is for longer-lived data.
   OB_MEM_DATA,
 
   /// The allocation is for an @ref ob_Obj value.
   OB_MEM_OBJECT,
 } ob_MemUsage;
 
+/// A *page* or block of memory provided by the system.
 typedef struct {
+  /// The size of this page.
   uint32_t size;
+
+  /// An offset "separating" assigned and unused data from unassigned memory
+  /// within this page.
+  uint32_t bump;
   char data[];
 } ob_Page;
 
-typedef ob_Address (*ob_FnMemAllocate)(void *self, ob_MemUsage usage,
-                                       size_t requested);
-typedef ob_Address (*ob_FnMemReallocate)(void *self, ob_Address source,
-                                         size_t new_size);
-typedef void (*ob_FnMemDeallocate)(void *self, ob_Address source);
+typedef struct ob_Memory *ob_Mem;
 
-typedef void *(*ob_FnMemTranslateAddress)(void *self, ob_Address addr);
-typedef ob_Address (*ob_FnMemTranslatePointer)(void *self, void const *pointer);
+/** @brief Allocate managed memory, and return it's address.
+ *
+ * This has 4 cases:
+ * - `old == 0` and `new > 0` : Allocate at least @p new_size bytes.
+ * - `old > 0` and `new == 0` : Deallocate at this address.
+ * - `old != new` : Reallocate (shrink or grow) the allocation.
+ * - Otherwise, it returns the same address.
+ *
+ * @note
+ * - If any (de,re)allocation takes place, this must set the stats in @p self as
+ *   needed.
+ * - When deallocating or on allocation failure, it must return
+ *   @ref OB_ADDRESS_NULL.
+ * - If possible, it should return the same address on reallocation.
+ * - Zeroing out any freshly allocated space is done elsewhere.
+ * - The @p usage parameter is intended to be a hint, and can be safely ignored.
+ *
+ * @warning
+ * Do not resize any allocated pages containing data.
+ */
+typedef ob_Address (*ob_FnMemAllocate)(ob_Mem self, ob_Address source,
+                                       size_t old_size, size_t new_size,
+                                       ob_MemUsage usage);
 
-typedef ob_Page *(*ob_FnMemGetPage)(void *self, ob_Address addr);
+/** @brief Translate an address into a pointer.
+ *
+ * @returns A pointer such that, when translated back into an address, returns
+ * @p addr, or @c NULL if it is equal to @ref OB_ADDRESS_NULL.
+ */
+typedef void *(*ob_FnMemTranslateAddress)(ob_Mem self, ob_Address addr);
 
-typedef struct {
+/** @brief Translate a pointer into an address.
+ *
+ * @sa ob_FnMemTranslateAddress
+ */
+typedef ob_Address (*ob_FnMemTranslatePointer)(ob_Mem self,
+                                               void const *pointer);
+
+/// Obtain the page allocated for this address.
+typedef ob_Page *(*ob_FnMemGetPage)(ob_Mem self, ob_Address addr);
+
+/** @brief Perform a garbage collection, from an address "forward".
+ *
+ * @note
+ * - The marking process is done independently of this.
+ * - It must not perform a collection on addresses lower than @p from. It is not
+ *   guaranteed that objects on these addresses are marked properly.
+ * - Any collected live objects must have their mark cleared, and their
+ *   finalizer must be called when destroyed (@ref obobj_destroy in
+ *   core/Object.h)
+ */
+typedef void (*ob_FnMemCollect)(ob_Mem self, ob_Address from);
+
+/// A memory allocator and garbage collector.
+struct ob_Memory {
   ob_FnMemAllocate allocate;
-  ob_FnMemDeallocate deallocate;
-  ob_FnMemReallocate reallocate;
 
-  ob_FnMemTranslateAddress translate_address;
-  ob_FnMemTranslatePointer translate_pointer;
+  struct {
+    ob_FnMemTranslateAddress address;
+    ob_FnMemTranslatePointer pointer;
+  } translate;
 
   ob_FnMemGetPage get_page;
 
+  ob_FnMemCollect collect;
+
   struct {
+    /// A measure of all currently used (live and garbage) memory.
     size_t used;
+
+    /// A measure of all currently unassigned memory.
     size_t total;
   } stats;
 
   void *userdata;
-} ob_Memory;
+};
 
 constexpr uint64_t OB_ADDRESS_MASK = (1ULL << 53) - 1;
 constexpr uint64_t OB_MAX_PAGE_SIZE = (1ULL << 32) - 1;
 constexpr uint64_t OB_MAX_PAGE_INDEX = (1ULL << 21) - 1;
 
 constexpr uint64_t OB_MIN_PAGE_SIZE = 8192;
-static_assert(OB_MIN_PAGE_SIZE >= sizeof("ob_PageHeader"));
+static_assert(OB_MIN_PAGE_SIZE >= sizeof(ob_Page));
+
+constexpr ob_Address OB_ADDRESS_NULL = 0;
+
+ob_Address obmem_allocate(ob_Mem self, ob_Address source, size_t old_size,
+                          size_t new_size, ob_MemUsage usage);
+void *obmem_translate_address(ob_Mem self, ob_Address addr);
+ob_Address obmem_translate_pointer(ob_Mem self, void const *pointer);
+ob_Page *obmem_get_page(ob_Mem self, ob_Address addr);
+void obmem_collect(ob_Mem self, ob_Address from);
 
 ob_Address obmem_address_create(uint32_t page, uint32_t offset);
 uint64_t obmem_get_page_offset(ob_Address addr);
--- core/src/Memory.c
+++ core/src/Memory.c
@@ -1,5 +1,59 @@
 #include <ob/core/Memory.h>
 
+#include <ql/Assert.h>
+
+#include <string.h>
+
+ob_Address obmem_allocate(ob_Mem self, ob_Address source, size_t old_size,
+                          size_t new_size, ob_MemUsage usage) {
+  QL_ASSERT(self->allocate != nullptr, "unexpected null");
+
+  ob_Address addr = self->allocate(self, source, old_size, new_size, usage);
+
+  if (new_size != 0) {
+    QL_ASSERT(addr != OB_ADDRESS_NULL, "allocation of %zu bytes failed",
+              new_size);
+
+    char *mem = obmem_translate_address(self, addr);
+
+    if (new_size > old_size) {
+      memset(mem + old_size, 0, new_size - old_size);
+    }
+  }
+
+  return addr;
+}
+
+void *obmem_translate_address(ob_Mem self, ob_Address addr) {
+  QL_ASSERT(self->translate.address != nullptr, "unexpected null");
+
+  if (addr == OB_ADDRESS_NULL) {
+    return nullptr;
+  }
+
+  return self->translate.address(self, addr);
+}
+
+ob_Address obmem_translate_pointer(ob_Mem self, void const *pointer) {
+  QL_ASSERT(self->translate.pointer != nullptr, "unexpected null");
+
+  if (pointer == nullptr) {
+    return OB_ADDRESS_NULL;
+  }
+
+  return self->translate.pointer(self, pointer);
+}
+
+ob_Page *obmem_get_page(ob_Mem self, ob_Address addr) {
+  QL_ASSERT(self->get_page != nullptr, "unexpected null");
+  return self->get_page(self, addr);
+}
+
+void obmem_collect(ob_Mem self, ob_Address from) {
+  QL_ASSERT(self->collect != nullptr, "unexpected null");
+  self->collect(self, from);
+}
+
 ob_Address obmem_address_create(uint32_t page, uint32_t offset) {
   QL_ASSERT(page <= OB_MAX_PAGE_INDEX, "page index larger than allowed");
   QL_ASSERT(offset <= OB_MAX_PAGE_SIZE, "page offset larger than page size");
